8ad50a9f845b5603e23091ea844c874ef4d96518
Massu <massu.queirantes@yahoo.com.br>
2025-03-28 11:03:41 -0300

    fix: "Consumindo o projeto"

--- Desafio_Sudoku/Sudoku/src/br/com/dio/Main.java
+++ Desafio_Sudoku/Sudoku/src/br/com/dio/Main.java
@@ -1,8 +1,21 @@
 package br.com.dio;
 
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+import br.com.dio.model.Board;
+
 public class Main {
 
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private static Board board;
+
+    private final static int BOARD_LIMIT = 9;
+
     public static void main(String[] args) {
+
+        final var positions = Stream.of(args);
 
     }
 }
