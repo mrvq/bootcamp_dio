79a57175a0a751c0861ccfcc10e2962c7c3ce5b0
Massu <massu.queirantes@yahoo.com.br>
2025-05-05 21:08:48 -0300

    fix:"implementacao da camada de dominio"

---
+++ API/API_Restfull_deploy/Bradesco-Java-Cloud/src/main/java/dio_bradesco_java/model/News.java
@@ -0,0 +1,8 @@
+package dio_bradesco_java.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ API/API_Restfull_deploy/Bradesco-Java-Cloud/src/test/java/dio_bradesco_java/BradescoJavaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dio_bradesco_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoJavaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
